241cfcc0ca08105b38adb805798d83dfbe261e34
Aboobacker MK <aboobackervyd@gmail.com>
2017-09-17 01:30:13 +0530

    updated with new luis end point

--- spec/luis_spec.rb
+++ spec/luis_spec.rb
@@ -4,4 +4,12 @@ describe Luis do
   it 'has a version number' do
     expect(Luis::VERSION).not_to be nil
   end
+
+  it '.configure' do
+    Luis.configure do |config|
+      config.subscription_key = '1234'
+    end
+
+    expect(Luis.subscription_key).to be_eql('1234')
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,8 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'luis'
+require 'vcr'
+
+VCR.configure do |config|
+  config.cassette_library_dir = "fixtures/vcr_cassettes"
+  config.hook_into :webmock
+end
